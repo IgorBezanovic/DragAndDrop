e97670a5645fcb8b2a81af4932837d3a65a41df7
Igor Bežanović <i.bezanovic@levi9.com>
2022-03-03 16:12:05 +0100

    Server try

--- src/containers/Info/Info.tsx
+++ src/containers/Info/Info.tsx
@@ -4,7 +4,7 @@ const Info: React.FC = () => {
   return (
     <div>
       <h1>Info</h1>
-      <form action="../../post" method="post" 
+      <form action="http://localhost:8080/post" method="post" 
               className="form">
           <button type="submit">Connected?</button>
         </form>
